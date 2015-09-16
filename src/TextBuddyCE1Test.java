7915954ba8e04c7c104e80a625d8c1f4d52a94eb
Harish V <harishv207@gmail.com>
2015-09-17 01:06:46 +0800

    wrote test case for deleting from an empty file

--- src/TextBuddyCE1Test.java
+++ src/TextBuddyCE1Test.java
@@ -35,12 +35,5 @@ public class TextBuddyCE1Test {
 		// delete from an empty file
 		TextBuddy.runCommand(TextBuddy.CommandType.CLEAR, "clear");
 		assertEquals(fileName + " is empty", TextBuddy.runCommand(TextBuddy.CommandType.DELETE, "delete 1"));
-		
-		// delete an invalid line
-		TextBuddy.runCommand(TextBuddy.CommandType.ADD, "add Performing unit testing now");
-		assertEquals("You have provided an invalid line number.", TextBuddy.runCommand(TextBuddy.CommandType.DELETE, "delete 2"));
-		
-		// delete a valid line
-		assertEquals("deleted from " + fileName + ": \"Performing unit testing now\"", TextBuddy.runCommand(TextBuddy.CommandType.DELETE, "delete 1"));
 	}
 }
