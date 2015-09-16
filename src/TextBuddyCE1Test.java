492e2c702e7f49cb6e8d6b3867b3dd2bee7845c2
Harish V <harishv207@gmail.com>
2015-09-17 00:49:32 +0800

    completed test cases for add command

--- src/TextBuddyCE1Test.java
+++ src/TextBuddyCE1Test.java
@@ -17,5 +17,9 @@ public class TextBuddyCE1Test {
 	@Test
 	public void testAdd() throws IOException {
 		assertEquals("added to mytextfile.txt: \"This is TextBuddy\"", TextBuddy.runCommand(TextBuddy.CommandType.ADD,"add This is TextBuddy"));
+		assertEquals("added to mytextfile.txt: \"TextBuddy is Great!\"", TextBuddy.runCommand(TextBuddy.CommandType.ADD,"add TextBuddy is Great!"));
+		assertEquals("added to mytextfile.txt: \"TextBuddy is amazing\"", TextBuddy.runCommand(TextBuddy.CommandType.ADD,"add TextBuddy is amazing"));
+		assertEquals("added to mytextfile.txt: \"With Great Power Comes Great Responsibility\"", TextBuddy.runCommand(TextBuddy.CommandType.ADD,"add With Great Power Comes Great Responsibility"));
+		assertEquals("added to mytextfile.txt: \"Patience is Virtue\"", TextBuddy.runCommand(TextBuddy.CommandType.ADD,"add Patience is Virtue"));
 	}
 }
