eeb8dafccb945e0ab9db0e03c355bc606e7515cc
Harish V <harishv207@gmail.com>
2015-09-18 23:11:22 +0800

    improved code readability for test file

--- src/TextBuddyTest.java
+++ src/TextBuddyTest.java
@@ -84,7 +84,9 @@ public class TextBuddyTest {
 				myTextBuddy.runCommand(TextBuddy.CommandType.SORT, "sort"));
 		
 		// test sort - if the contents have also been sorted properly
-		assertEquals("1. Buffalo\n2. Camel\n3. Elephant\n4. Flamingo\n5. Gorilla\n6. Hippopotamus\n7. Koala\n8. Meerkat\n9. Mouse\n10. Penguin\n11. Rabbit\n12. Reindeer\n13. Shark\n14. Zebra", 
+		assertEquals("1. Buffalo\n2. Camel\n3. Elephant\n4. Flamingo\n5. Gorilla\n6."
+				+ "Hippopotamus\n7. Koala\n8. Meerkat\n9. Mouse\n10. Penguin\n11." 
+				+ "Rabbit\n12. Reindeer\n13. Shark\n14. Zebra", 
 				myTextBuddy.runCommand(TextBuddy.CommandType.DISPLAY, "display"));
 		
 		// test search method - on a non-empty file
